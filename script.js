5f40421faa2d91ac91f0adb44f08dddaa141035a
AutumnCharnesky <autumn.breeze.c18@gmail.com>
2017-04-14 14:13:38 -0400

    another one

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
  console.log("Hello");
-
-var temperature = -03;
+//temp
+var temperature = 48;
 
 console.log(((temperature - 32)/1.8)+ " degrees");
 
-
+//fizzbuzz
 for (var i=1; i <= 100; i++) {
 
 if (i % 3 === 0 && i % 5 ===0) {
@@ -17,3 +17,49 @@ if (i % 3 === 0 && i % 5 ===0) {
   console.log(i);
 }
 }
+
+//clothing event
+var event = ""
+var clothing = ""
+var coat = ""
+var temp = ""
+
+if (event === "casual") {
+  clothing = "somthing comfy";
+} else if (event === "semi-formal") {
+  clothing = "a button up";
+} esle (event === formal) {
+  clothing = "a suit";
+}
+
+if (temp  < 60 ) {
+  coat = "no jacket";
+}
+if (temp > 60 ) {
+  coat = "";
+}
+if (temp > 70 || temp <54 ) {
+  coat = "";
+}
+
+console.log("since it is " + temp + clothing + coat);
+
+//loops
+for( var i=1; i<=10; i++) {
+console.log("It's Friday!!!");
+}
+
+var number;
+
+do {
+  number = prompt("what number am I thinking of between 1-10?");
+} while(number !== 6);
+console.log("you got it!");
+
+//array
+var numbers = [1,1,2,3,5,8];
+var total = 0;
+numbers.forEach(function(i){
+  total +=i
+});
+  console.log(total);
