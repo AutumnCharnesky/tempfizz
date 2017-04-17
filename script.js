c34e727388c8b4014ce95ed88c311dc849ff4ccd
AutumnCharnesky <autumn.breeze.c18@gmail.com>
2017-04-17 10:17:00 -0400

    challenge

--- script.js
+++ script.js
@@ -1,65 +1,84 @@
- console.log("Hello");
-//temp
-var temperature = 48;
+//  console.log("Hello");
+// //temp
+// var temperature = 48;
+//
+// console.log(((temperature - 32)/1.8)+ " degrees");
+//
+// //fizzbuzz
+// for (var i=1; i <= 100; i++) {
+//
+// if (i % 3 === 0 && i % 5 ===0) {
+//   console.log("FizzBuzz");
+// } else if (i % 3 === 0) {
+//   console.log("Fizz");
+// } else if (i % 5 === 0) {
+//   console.log("Buzz");
+// } else {
+//   console.log(i);
+// }
+// }
+//
+// //clothing event
+// var event = ""
+// var clothing = ""
+// var coat = ""
+// var temp = ""
+//
+// if (event === "casual") {
+//   clothing = "somthing comfy";
+// } else if (event === "semi-formal") {
+//   clothing = "a button up";
+// } esle (event === formal) {
+//   clothing = "a suit";
+// }
+//
+// if (temp  < 60 ) {
+//   coat = "no jacket";
+// }
+// if (temp > 60 ) {
+//   coat = "";
+// }
+// if (temp > 70 || temp <54 ) {
+//   coat = "";
+// }
+//
+// console.log("since it is " + temp + clothing + coat);
+//
+// //loops
+// for( var i=1; i<=10; i++) {
+// console.log("It's Friday!!!");
+// }
+//
+// var number;
+//
+// do {
+//   number = prompt("what number am I thinking of between 1-10?");
+// } while(number !== 6);
+// console.log("you got it!");
+//
+// //array
+// var numbers = [1,1,2,3,5,8];
+// var total = 0;
+// numbers.forEach(function(i){
+//   total +=i
+// });
+//   console.log(total);
 
-console.log(((temperature - 32)/1.8)+ " degrees");
+// //longer name
+// var name1="Kim Possible";
+// var name2="Ron Stoppable";
+//
+// if(name1.length < name2.length) {
+//   console.log("Ron has the longer name.");
+// } else {
+//   console.log("Kim has the longer name.");
+// }
 
-//fizzbuzz
-for (var i=1; i <= 100; i++) {
-
-if (i % 3 === 0 && i % 5 ===0) {
-  console.log("FizzBuzz");
-} else if (i % 3 === 0) {
-  console.log("Fizz");
-} else if (i % 5 === 0) {
-  console.log("Buzz");
-} else {
-  console.log(i);
-}
-}
-
-//clothing event
-var event = ""
-var clothing = ""
-var coat = ""
-var temp = ""
-
-if (event === "casual") {
-  clothing = "somthing comfy";
-} else if (event === "semi-formal") {
-  clothing = "a button up";
-} esle (event === formal) {
-  clothing = "a suit";
-}
-
-if (temp  < 60 ) {
-  coat = "no jacket";
-}
-if (temp > 60 ) {
-  coat = "";
-}
-if (temp > 70 || temp <54 ) {
-  coat = "";
-}
-
-console.log("since it is " + temp + clothing + coat);
-
-//loops
-for( var i=1; i<=10; i++) {
-console.log("It's Friday!!!");
-}
-
-var number;
-
-do {
-  number = prompt("what number am I thinking of between 1-10?");
-} while(number !== 6);
-console.log("you got it!");
-
-//array
-var numbers = [1,1,2,3,5,8];
-var total = 0;
-numbers.forEach(function(i){
-  total +=i
-});
-  console.log(total);
+// //sum of
+// var numbers=[1,1,2,3,5,8];
+// var sum=0
+//
+// numbers.forEach(function(num){
+//   (sum += num);
+// 
+// }); console.log(sum);
